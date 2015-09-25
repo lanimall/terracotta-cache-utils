b20c76fa9fbdba179e3c1493a0a91811304e9364
Fabien Sanglier <fabiensanglier@gmail.com>
2015-09-25 11:16:11 -0400

    updates / fixes

--- README.md
+++ README.md
@@ -4,7 +4,42 @@ ehcache-tools
 Operations tool set for terracotta / ehcache. 
 
 This package contains handy tools to help operate teams to answer typical queries like
-1. Is it in cache.
-2. What is the size of my cache
-3. Can you check if this business key exists. 
+1. How many objects are in my cache?
+2. Print all keys( strings )  and possible values ( strings)
+3. Can you check if this business key exists.
+
+
+Build
+The project is maven based and have few profiles for various versions of ehcache / terracotta. Review the pom.xml for the profiles and build.
+
+To create a tar package
+mvn clean install 
+
+this will create a ehcache-tools-bin.tar.gz which can be deployed on any host. 
+
+
+Installation
+1. Unzip ehcache-tools-bin.tar.gz to any directory. (TOOLS_HOME). 
+2. Copy terracotta license file into TOOLS_HOME/config
+3. Edit/Copy the ehcache.xml file into TOOLS_HOME/config. 
+
+
+Tools Provided
+
+ehcacheping : Ping utility to check health of terracotta cluster. This is a 2 step process. Loads abut 10K entries to a cache , disconnect and then read 10K entires back and verifies. 
+
+Usage tcPing  [1/2] [unique id] [number of elements]
+tcPing needs to be executed in a 2 step process. The first argument 1 is to load and 2 is retrieve and avalidate
+
+
+ehcachecli is a command line cli with following utilities
+
+./ehcachecli.sh -h
+Syntax: ./ehcachecli.sh [cacheKeyValuePrint|cacheKeysPrint|cacheSize] [arguments.....]
+cacheKeyValuePrint - Prints the Keys and values (only string or list/string) for a given cache.
+cacheKeysPrint - Prints the Keys in a cache or all caches.
+cacheSize - Prints the total number of cache entries in each cache in a continous loop.
+tcPing - Health Check of the cluster
+
+
 
--- scripts/ehcacheping.sh
+++ scripts/ehcacheping.sh
@@ -1,159 +1,48 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
 #
 
-<<<<<<< HEAD
-# resolve links - $0 may be a softlink
-PRG="$0"
-
-while [ -h "$PRG" ]; do
-  ls=`ls -ld "$PRG"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '/.*' > /dev/null; then
-    PRG="$link"
-  else
-    PRG=`dirname "$PRG"`/"$link"
-  fi
-done
-
-PRGDIR=`dirname "$PRG"`
-BASEDIR=`cd "$PRGDIR/.." > /dev/null; pwd`
-
-if [ -f "${BASEDIR}/bin/setenv-tcping.sh" ]; then
-  . "${BASEDIR}/bin/setenv-tcping.sh"
-fi
-
-if [ "x$TC_CONNECT_URL" == "x" ]; then
-  echo "Error: terracotta_connect_url is not defined correctly." 1>&2
-  echo "  We cannot execute $JAVACMD" 1>&2
-  exit 1
-fi
-=======
 BASE_DIR=`dirname "$0"`/..
->>>>>>> gbshiva/master
 
 # OS specific support.  $var _must_ be set to either true or false.
-cygwin=false;
-darwin=false;
+cygwin=false
 case "`uname`" in
-  CYGWIN*) cygwin=true ;;
-  Darwin*) darwin=true
-           if [ -z "$JAVA_VERSION" ] ; then
-             JAVA_VERSION="CurrentJDK"
-           else
-             echo "Using Java version: $JAVA_VERSION"
-           fi
-           if [ -z "$JAVA_HOME" ] ; then
-             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
-           fi
-           ;;
+CYGWIN*) cygwin=true;;
 esac
 
-if [ -z "$JAVA_HOME" ] ; then
-  if [ -r /etc/gentoo-release ] ; then
-    JAVA_HOME=`java-config --jre-home`
-  fi
+if test \! -d "${JAVA_HOME}"; then
+  echo "$0: the JAVA_HOME environment variable is not defined correctly"
+  exit 2
 fi
 
-# For Cygwin, ensure paths are in UNIX format before anything is touched
-if $cygwin ; then
-  [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
-  [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
-fi
-
-# If a specific java binary isn't specified search for the standard 'java' binary
-if [ -z "$JAVACMD" ] ; then
-  if [ -n "$JAVA_HOME"  ] ; then
-    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
-      # IBM's JDK on AIX uses strange locations for the executables
-      JAVACMD="$JAVA_HOME/jre/sh/java"
-    else
-      JAVACMD="$JAVA_HOME/bin/java"
-    fi
-  else
-    JAVACMD=`which java`
-  fi
-fi
-
-if [ ! -x "$JAVACMD" ] ; then
-  echo "Error: JAVA_HOME is not defined correctly." 1>&2
-  echo "  We cannot execute $JAVACMD" 1>&2
-  exit 1
-fi
-
-if [ -z "$LIBS" ]
-then
-  LIBS="$BASEDIR"/libs
-fi
-
-if [ -z "$CONFIG" ]
-then
-  CONFIG="$BASEDIR"/config
-fi
-
-CLASSPATH=$CLASSPATH_PREFIX:"$CONFIG":"$LIBS"/*
-
-# For Cygwin, switch paths to Windows format before running java
+# For Cygwin, convert paths to Windows before invoking java
 if $cygwin; then
-  [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
-  [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
-  [ -n "$HOME" ] && HOME=`cygpath --path --windows "$HOME"`
-  [ -n "$BASEDIR" ] && BASEDIR=`cygpath --path --windows "$BASEDIR"`
-  [ -n "$LIBS" ] && LIBS=`cygpath --path --windows "$LIBS"`
-  [ -n "$CONFIG" ] && CONFIG=`cygpath --path --windows "$CONFIG"`
+  [ -n "$BASE_DIR" ] && BASE_DIR=`cygpath -d "$BASE_DIR"`
 fi
 
-JAVA_OPTS="${JAVA_OPTS} -Xms64m -Xmx128m -Dtc.connect.servers=${TC_CONNECT_URL} -Dehcache.config.path=${CONFIG}/ehcache-ping.xml"
-
-#if not defined or does not exist, try in the base directory
-if [ -f ${TC_LICENSEKEY} ]; then
-  JAVA_OPTS="${JAVA_OPTS} -Dcom.tc.productkey.path=${TC_LICENSEKEY}"
-fi
-
-<<<<<<< HEAD
-=======
 PERF_CLASSPATH=$(echo ${BASE_DIR}/lib/*.jar | tr ' ' ':')
 PERF_CLASSPATH=${PERF_CLASSPATH}:${BASE_DIR}/config/
 JAVA_OPTS="${JAVA_OPTS} -Xms128m -Xmx512m -XX:MaxDirectMemorySize=10G -Dcom.tc.productkey.path=${BASE_DIR}/config/terracotta-license.key"
->>>>>>> gbshiva/master
 dt=`date +%Y%m%d_%H%M%S`
-
-echo
-echo "####### Executing Step 1 of Ping: Putting objects in cache"
-echo
-
-$JAVACMD $JAVA_OPTS \
-   -classpath "$CLASSPATH" \
-   -Dbasedir="$BASEDIR" \
-   com.terracotta.tools.tcPing 1 $dt 10000
-
+# Execute Step 1 of Ping
+${JAVA_HOME}/bin/java -cp $PERF_CLASSPATH  com.terracotta.tools.tcPing 1 $dt 10000
 return_code=$?
-if [ $return_code == 0 ] ; then
-    echo "####### Executing Step 2 of Ping: Retrieving all objects in cache"
-    echo
-
-    $JAVACMD $JAVA_OPTS \
-       -classpath "$CLASSPATH" \
-       -Dbasedir="$BASEDIR" \
-       com.terracotta.tools.tcPing 2 $dt 10000
-
-    return_code=$?
-    if [ $return_code == 0 ] ; then
-       echo
-       echo "####### Terracotta Ping Successful!!"
-       echo
-       exit 0
-    else # Step 2 failed
-       echo
-       echo "####### Terracotta Ping failed!!"
-       echo
-       exit 1
-    fi
+if [ $return_code == 0 ]
+then
+# Execute Step 2 of Ping
+	${JAVA_HOME}/bin/java -cp $PERF_CLASSPATH  com.terracotta.tools.tcPing 2 $dt 10000
+        return_code=$?
+        if [ $return_code == 0 ]
+        then
+# Step 2 succesful
+        exit 0
+# Step 2 failed
+        else
+        exit 1
+        fi
 else
-    echo
-    echo "####### Terracotta Ping failed!!"
-    echo
-    exit 1
+# Step 1 failed
+exit 1
 fi
